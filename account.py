a6b620675e9ffb356955b200ef31583e559874fa
Mauricio Tovar Gil <mauricio.tovar.gil@gmail.com>
2023-06-03 18:36:10 -0500

    v1.01

--- account.py
+++ account.py
@@ -2,8 +2,8 @@ from reto1 import AccountService
 #IMPLENT INTERFACE AccountService
 class Account(AccountService):
     def deposit(self, amount: int):
-        raise NotImplementedError("error")
+        return amount
     def withdraw(self, amount: int):
-        raise NotImplementedError("error")
+        return amount
     def printStatement(self):
-        raise NotImplementedError("error")
+        pass
--- test_feature.py
+++ test_feature.py
@@ -5,24 +5,23 @@ from account import Account
 class TestFeature(unittest.TestCase):    
     
     @patch('builtins.print')
-    def test_printStatement(self):
+    def test_printStatement(self,mock):
         
-        mock = MagicMock()
-        
-        Account.deposit(4000)
-        Account.withdraw(400)
-        Account.deposit(1000)
+        Account.deposit(self, 4000)
+        Account.withdraw(self, 400)
+        Account.deposit(self, 1000)
 
+        mock = MagicMock()
         with patch('time.strftime', return_value='14/01/2012'):
-            Account.printStatement()
+            Account.printStatement(self)
             mock.assert_called_with("14/01/2012 || 1000   || 4600")
 
         with patch('time.strftime', return_value='13/01/2012'):
-            Account.printStatement()
+            Account.printStatement(self)
             mock.assert_called_with("13/01/2012 || -400   || 3600")
 
         with patch('time.strftime', return_value='10/01/2012'):
-            Account.printStatement()
+            Account.printStatement(self)
             mock.assert_called_with("10/01/2012 || 4000   || 4000")
 
 if __name__ == '__main__':
