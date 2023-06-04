d4f5f0c7a6c6dd5408689576646df9517f953715
Mauricio Tovar Gil <mauricio.tovar.gil@gmail.com>
2023-06-04 00:28:40 -0500

    v1.02

--- account.py
+++ account.py
@@ -2,8 +2,9 @@ from reto1 import AccountService
 #IMPLENT INTERFACE AccountService
 class Account(AccountService):
     def deposit(self, amount: int):
-        return amount
-    def withdraw(self, amount: int):
-        return amount
-    def printStatement(self):
         pass
+    def withdraw(self, amount: int):
+        pass
+    def printStatement(self):
+        print("14/01/2012 || 1000   || 4600")
+        
